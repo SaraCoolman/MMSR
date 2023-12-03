7d3a2eec5267f3088ecf495dbba7d34f2d5f0071
SaraCoolman <sara.scheucher@gmail.com>
2023-12-03 10:03:56 +0100

    precision recall

--- project_2_team_E/ret.py
+++ project_2_team_E/ret.py
@@ -3,9 +3,14 @@ import pandas as pd
 import statistics as st
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.pyplot as plt
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 '''
-Version: 1.0.1 
-Date: 14.11.2023
+Version: 2.0.1 
+Date: 26.11.2023
 '''
 '''
 Utility Functions 
@@ -77,12 +82,23 @@ def get_genre_from_ids(ids, genres):
             res.append((entry.iloc[0]['id'],entry.iloc[0]['genre']))
     return res
 
+
+'''
+function to get the id and genres from the id of the query track
+info - pandas Dataframe - information of genres
+res - list[str] - list which stores the genres from the id of the query track
+'''
 def get_genre_from_query(query_id, genres):
     entry = genres[genres['id'] == query_id]
     if not entry.empty:
-        return [(entry.iloc[0]['id'], entry.iloc[0]['genre'])]
+        res = [(entry.iloc[0]['id'], entry.iloc[0]['genre'])]
     else:
-        return []
+        res = []
+    return res
+
+'''
+function to calculate precision 
+'''
     
 def calculate_precision(query_genre, retrieved_genres):
     query_genres = set(eval(query_genre[0][1]))
@@ -95,9 +111,30 @@ def calculate_precision(query_genre, retrieved_genres):
     precision = count / N
     return precision
 
+
+def calculate_precision_at_k(query_genre, retrieved_genres, k):
+    query_genres = set(eval(query_genre[0][1]))
+    
+    # Sort retrieved_genres by some relevance score or confidence measure
+    sorted_retrieved_genres = sorted(retrieved_genres, key=lambda x: x[1], reverse=True)[:k]
+
+    count = 0
+    for song_id, genres_str in sorted_retrieved_genres:
+        genres = set(eval(genres_str))
+        if any(genre in genres for genre in query_genres):
+            count += 1
+
+    precision_at_k = count / k
+    return precision_at_k
+
+
+
+'''
+function that counts relevant songs according to the query song in the whole dataset
+'''
+
 def count_relevant_songs_in_dataset(query_genre, dataset_genres):
     query_genres = set(eval(query_genre[0][1]))
-    N = len(dataset_genres)
     count = 0
     for song_id, genres_str in dataset_genres:
         genres = set(eval(genres_str))
@@ -106,10 +143,11 @@ def count_relevant_songs_in_dataset(query_genre, dataset_genres):
     count
     return count
 
-
+'''
+function that counts relevant songs according to the query song in the retrieved result
+'''
 def count_relevant_songs_in_result(query_genre, retrieved_genres):
     query_genres = set(eval(query_genre[0][1]))
-    N = len(retrieved_genres)
     count = 0
     for song_id, genres_str in retrieved_genres:
         genres = set(eval(genres_str))
@@ -119,10 +157,11 @@ def count_relevant_songs_in_result(query_genre, retrieved_genres):
     return count
 
 
-
+'''
+function that calculates the recall
+'''
 def calculate_recall(query_genre, retrieved_genres, dataset_genres):
     query_genres = set(eval(query_genre[0][1]))
-    N = len(retrieved_genres)
     relevant_retrieved_songs = 0
     for song_id, genres_str in retrieved_genres:
         genres = set(eval(genres_str))
@@ -130,7 +169,6 @@ def calculate_recall(query_genre, retrieved_genres, dataset_genres):
             relevant_retrieved_songs += 1
     
     query_genres = set(eval(query_genre[0][1]))
-    N2 = len(dataset_genres)
     relevant_songs_dataset = 0
     for song_id, genres_str in dataset_genres:
         genres = set(eval(genres_str))
@@ -139,6 +177,30 @@ def calculate_recall(query_genre, retrieved_genres, dataset_genres):
             
     return  relevant_retrieved_songs / relevant_songs_dataset
 
+
+
+
+def calculate_recall_at_k(query_genre, retrieved_genres, dataset_genres, k):
+    query_genres = set(eval(query_genre[0][1]))
+    
+    # Consider only the top k retrieved songs
+    top_k_retrieved_genres = retrieved_genres[:k]
+
+    relevant_retrieved_songs = 0
+    for song_id, genres_str in top_k_retrieved_genres:
+        genres = set(eval(genres_str))
+        if any(genre in genres for genre in query_genres):
+            relevant_retrieved_songs += 1
+    
+    query_genres = set(eval(query_genre[0][1]))
+    relevant_songs_dataset = 0
+    for song_id, genres_str in dataset_genres:
+        genres = set(eval(genres_str))
+        if any(genre in genres for genre in query_genres):
+            relevant_songs_dataset += 1
+            
+    return relevant_retrieved_songs / relevant_songs_dataset
+
 '''
 function to calculate the average precision of all 3 query songs
 '''
@@ -146,6 +208,8 @@ def average_precision(p1, p2, p3):
     precisions = [p1,p2,p3]
     average_precision = st.mean(precisions)
     return average_precision
+
+
 
 '''
 function to calculate the average recall of all 3 query songs
@@ -156,26 +220,35 @@ def average_recall(r1, r2, r3):
     return average_recall
 
 
-def compute_genre_distribution(query_genre, retrieved_result):
+
+def compute_genre_distribution(retrieved_result, dataset_genres):
+    # Get unique genres in the dataset
     all_genres = set()
+    for _, genres_str in dataset_genres:
+        all_genres.update(eval(genres_str))
 
-    query_genres = set(eval(query_genre[0][1]))
-    for genre in query_genres:
-        all_genres.add(genre)
-
+    # Initialize genre distribution vector with zeros
     genre_distribution = {genre: 0.0 for genre in all_genres}
 
-    for genre in query_genres:
-        genre_distribution[genre] += 1.0
-
+    # Update genre distribution based on the retrieved tracks
+    total_tracks = len(retrieved_result)
     for _, retrieved_genres_str in retrieved_result:
         retrieved_genres = set(eval(retrieved_genres_str))
         for genre in retrieved_genres:
-            if genre in all_genres:
-                genre_distribution[genre] += 1.0 / len(retrieved_result)
+            genre_distribution[genre] += 1.0 / total_tracks if total_tracks > 0 else 0.0
 
+    # Convert the genre distribution to a list
     genre_distribution_list = [genre_distribution[genre] for genre in all_genres]
-    return genre_distribution_list
+    
+     # Normalize the genre distribution
+    normalized_distribution = {genre: count / total_tracks for genre, count in genre_distribution.items()}
+
+    # Calculate Shannon's entropy for genre diversity@10
+    entropy = -sum(p * math.log2(p) for p in normalized_distribution.values() if p > 0)
+
+    return normalized_distribution, entropy
+
+
 
 '''
 function to print the info from a list
@@ -215,6 +288,39 @@ def audio_based(id, repr, N, sim_func):
     res = [id for id, _ in most_similar_tracks]
 
     return res 
+
+
+'''
+retrieval system for a representation 
+id - str - id of the song in the query
+repr - pandas Dataframe - representation of lyrics
+N - int - number of retrieved tracks  
+sim_func - func - similarity function 
+
+returns - list[str]
+track_ids - ids of tracks retrieved 
+'''
+def text_based(id, repr, N, sim_func):
+
+    #search for the row of the query song in the representation and get the vector of the query song
+    query_row = repr[repr['id'] == id]
+    query_vec = query_row.iloc[:, 2:].values[0]
+
+    similarities = []
+
+    #iterate through all tracks in the representation dataset, compute similarity score, add song IDs and store to a list
+    for _ , row in repr.iterrows():
+        track_vec = row.iloc[2:].values
+        similarity = sim_func(query_vec, track_vec)
+        similarities.append((row['id'], similarity))
+
+    #sort by similarity score from most similar to least similar and save N most similar tracks and retrieve ids
+    similarities.sort(key=lambda x: x[1], reverse=True)
+    most_similar_tracks = similarities[1:N+1]
+    res = [id for id, _ in most_similar_tracks]
+
+    return res 
+
 
 '''
 return N tracks randomly
